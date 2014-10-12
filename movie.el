aafc7b0bc69b7b7db65e39ab3e5acf91a4d2319c
Lars Magne Ingebrigtsen <larsi@gnus.org>
2014-10-12 16:22:48 +0200

    Get the position from the stats file, if possible.
    This makes it easier to view the same files from different
    machines.

--- movie.el
+++ movie.el
@@ -394,6 +394,26 @@
   (movie-play-1 (append movie-player (list file))))
 
 (defun movie-find-position (file &optional no-skip)
+  (or (movie-find-position-from-stats file no-skip)
+      (movie-find-position-from-mplayer file no-skip)))
+
+(defun movie-find-position-from-stats (file &optional no-skip)
+  (let ((pos
+	 (car (plist-get (cdr
+			  (assoc
+			   (file-name-nondirectory file)
+			   (cdr (assoc 'tracks (movie-get-stats
+						(file-name-directory file))))))
+			 :seen))))
+    (cond
+     ((null pos)
+      nil)
+     (no-skip
+      (format "%s" pos))
+     (t
+      (format "%s" (max 0 (- pos 2)))))))
+
+(defun movie-find-position-from-mplayer (file &optional no-skip)
   (when (and (file-exists-p "~/.mplayer.positions")
 	     (string-match "^/tv\\|^/dvd\\|http:" file)
 	     (not (equal file "/tv/live")))
