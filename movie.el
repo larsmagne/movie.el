ca64d4e88acc51e20012f01a2db6f989dbea3421
Lars Magne Ingebrigtsen <larsi@gnus.org>
2015-05-13 01:48:24 +0200

    mplayer can play https now, so no need for insecure

--- movie.el
+++ movie.el
@@ -863,8 +863,7 @@
 
 (defun movie-direct-url (url)
   (with-temp-buffer
-    (call-process "youtube-dl" nil (current-buffer) nil
-		  "--prefer-insecure" "-g" url)
+    (call-process "youtube-dl" nil (current-buffer) nil "-g" url)
     (goto-char (point-min))
     (buffer-substring (point) (line-end-position))))
     
